a32babf9ca48689f5d0078c65963eaff1864920c
Gustavo Bozzano <gustavocbozzano@gmail.com>
2023-09-13 08:00:07 -0300

    .

---
+++ src/main/java/com/web2/web2springboottrabalho/Web2SpringbootTrabalhoApplication.java
@@ -0,0 +1,13 @@
+package com.web2.web2springboottrabalho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web2SpringbootTrabalhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web2SpringbootTrabalhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web2/web2springboottrabalho/Web2SpringbootTrabalhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web2.web2springboottrabalho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web2SpringbootTrabalhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
